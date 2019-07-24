ba05f4df8ec011ef17754c9e7a4a69bb07de91a5
xMathfreak <36758660+xMathfreak@users.noreply.github.com>
2019-07-24 18:42:31 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,89 +1,82 @@
 const Discord = require("discord.js");
 const client = new Discord.Client();
+const prefix = "s!"
+const admprefix = "s@"
 
-var prefix = "s!"
-var admprefix = "s@"
-
-client.on("message", (message) => {
-    // our new check:
-    if (!message.content.startsWith(prefix) || message.author.bot) return;
-    // [rest of the code]-
-  });
-  
+client.on('error', console.error);
 client.on('ready', () => {
   console.log(`Logged in as ${client.user.tag}!`);
-  client.user.setActivity("s!help | v0.0.3a");   
+  client.user.setActivity("s!help | v0.0.3.4a");
 });
 
 client.on("message", async message => {
-const args = message.content.slice(prefix.length).trim().split(/ +/g);
+      let cont = message.content.slice(prefix.length).split(" ");
+      let args = cont.slice(1);
 
-    
-    if (message.content === prefix+'help') {
-        message.reply(  "\n " +
-                    "Default prefix is `s!`" +
-                    "\n " +
-                    "\n " +
-                    "Commands:" +
-                    "\n`s!help` - Shows this menu" +
-                    "\n`s!ping` - Pings the bot" +
-                    "\n`s!flip` - Flips a coin"+
-                    "\n " +
-                    "\n Chat:" +
-                    "\n`henlo` - Shows a birb henlo");
-    }
- 
-    if (message.content === prefix+'ping') {
-        message.reply(new Date().getTime() - message.createdTimestamp + " ms");
-    }
-    
-    if (message.content === admprefix+'reset') {
+      if (command == "help") { // creates a command *help
+        var embedhelpmember = new Discord.RichEmbed() // sets a embed box to the variable embedhelpmember
+          .setTitle("**List of Commands**\n") // sets the title to List of Commands
+          .addField(" - help", "Displays this message (Correct usage: s!help)") // sets the first field to explain the command *help
+          .addField(" - ping", "Pings the bot (Correct usage: s!ping)") // sets the second field to explain the command *ping
+          .setColor(0xFFA500) // sets the color of the embed box to orange
+          .setFooter("You need help, do you?") // sets the footer to "You need help, do you?"
+      }
+
+      //Purge command
+      if (message.content.startsWith(prefix + 'purge')) {
+        async function purge() {
+          message.delete();
+
+          if (!message.member.hasPermission(['MANAGE_MESSAGES'])) {
+            message.channel.send('You do not have permission to use this command');
+            return;
+          }
+
+          if (isNaN(args[0])) {
+            message.channel.send('Please input a number');
+            return;
+          }
+
+          const fetched = await message.channel.fetchMessages({
+            limit: args[0]
+          });
+          console.log(fetched.size + 'message(s) found, deleting...');
+
+          message.channel.bulkDelete(fetched)
+            .catch(error => message.channel.send('Error: $(error)'));
+        }
+        purge();
+      }
+
+
+      if (message.content === prefix + 'ping') {
+        message.channel.send(new Date().getTime() - message.createdTimestamp + " ms");
+      }
+
+      if (message.content === admprefix + 'reset') {
+
+        if (!message.member.hasPermission(['ADMINISTRATOR'])) {
+          message.channel.send('You do not have permission to use this command')
+          return;
+        }
+
         resetBot(message.channel);
-    }
+      }
 
-    if (message.content === prefix+'prune') {
-        const amount = parseInt(args[0]) + 1;
-
-		if (isNaN(amount)) {
-			return message.reply('that doesn\'t seem to be a valid number.');
-		}
-		else if (amount <= 1 || amount > 100) {
-			return message.reply('you need to input a number between 1 and 99.');
-		}
-
-		message.channel.bulkDelete(amount, true).catch(err => {
-			console.error(err);
-			message.reply('there was an error trying to prune messages in this channel!');
-		});
-    }
-    
-    if (message.content === prefix+'server') {
-		message.reply(`Server name: ${message.guild.name}\nTotal members: ${message.guild.memberCount}`);
-    }
-    
-    if (message.content === prefix+'user-info') {
-		message.reply(`Your username: ${message.author.username}\nYour ID: ${message.author.id}`);
-    }
-    
-    if (message.content === 'Hey Sentry') {
-        message.reply('Hi');
-    }
-    
-    if (message.content === prefix+'flip') {
+      if (message.content === prefix + 'flip') {
         var result = Math.floor((Math.random() * 2) + 1);
-    	if (result == 1) {
-    		message.reply("The coin landed on heads");
-    	} else if (result == 2) {
-    		message.reply("The coin landed on tails");
-    	}
+        if (result == 1) {
+          message.reply("The coin landed on heads");
+        } else if (result == 2) {
+          message.reply("The coin landed on tails");
+        }
+
+      });
+
+    function resetBot(channel) {
+      channel.send('Rebooting Sentry...')
+        .then(message => client.destroy())
+        .then(() => client.login('NDQ0OTE5OTMwNTc2NDM3Mjg2.XThl-g.fDuDjpqzFiLjclROWhahsb277o4'));
     }
-});
 
-function resetBot(channel) {
-    // send channel a message that you're resetting bot [optional]
-    channel.send('Rebooting Sentry...')
-    .then(message => client.destroy())
-    .then(() => client.login('NDI0MjcyOTIzNzYzODAyMTEy.DeCoCg.tYOG6ede6Ii-1ci2oD2sN2qph9A'));
-}
-
-client.login('NDI0MjcyOTIzNzYzODAyMTEy.DeCoCg.tYOG6ede6Ii-1ci2oD2sN2qph9A');
+    client.login('NDQ0OTE5OTMwNTc2NDM3Mjg2.XThl-g.fDuDjpqzFiLjclROWhahsb277o4');
