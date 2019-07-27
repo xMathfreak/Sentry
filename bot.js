671a26c9a902149d4713951e2cc208ccfbfedd43
xMathfreak <36758660+xMathfreak@users.noreply.github.com>
2019-07-27 01:18:35 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,4 +1,6 @@
 const Discord = require("discord.js");
+const cheerio = require("cheerio");
+const request = require("request");
 const client = new Discord.Client();
 const prefix = "s!"
 const admprefix = "s@"
@@ -12,7 +14,7 @@ client.on('ready', () => {
 client.on("message", async message => {
   let cont = message.content.slice(prefix.length).split(" ");
   let args = cont.slice(1);
-
+  var parts = message.content.split(" ");
 
 //Purge command
 if (message.content.startsWith(prefix + 'purge')) {
@@ -40,6 +42,11 @@ if (message.content.startsWith(prefix + 'purge')) {
   purge();
 }
 
+if (parts[0] === "s!img") {
+
+  image(message, parts);
+
+}
 
 if (message.content === prefix+'ping') {
   message.channel.send("Time taken: " + (new Date().getTime() - message.createdTimestamp) + " ms");
@@ -57,10 +64,53 @@ if (message.content === admprefix+'reset') {
 
 });
 
+//Functions
 function resetBot(channel) {
     channel.send('Rebooting Sentry...')
     .then(message => client.destroy())
     .then(() => client.login(process.env.BOT_TOKEN));
+}
+
+function image(message, parts) {
+ 
+  /* extract search query from message */
+
+  var search = parts.slice(1).join(" "); // Slices of the command part of the array ["!image", "cute", "dog"] ---> ["cute", "dog"] ---> "cute dog"
+
+  var options = {
+      url: "http://results.dogpile.com/serp?qc=images&q=" + search,
+      method: "GET",
+      headers: {
+          "Accept": "text/html",
+          "User-Agent": "Chrome"
+      }
+  };
+  request(options, function(error, response, responseBody) {
+      if (error) {
+          // handle error
+          return;
+      }
+
+      /* Extract image URLs from responseBody using cheerio */
+
+      $ = cheerio.load(responseBody); // load responseBody into cheerio (jQuery)
+
+      // In this search engine they use ".image a.link" as their css selector for image links
+      var links = $(".image a.link");
+
+      // We want to fetch the URLs not the DOM nodes, we do this with jQuery's .attr() function
+      // this line might be hard to understand but it goes thru all the links (DOM) and stores each url in an array called urls
+      var urls = new Array(links.length).fill(0).map((v, i) => links.eq(i).attr("href"));
+      console.log(urls);
+      if (!urls.length) {
+          // Handle no results
+          return;
+      }
+
+      // Send result
+      message.channel.send( urls[0] );
+  });
+
 }
 
 client.login(process.env.BOT_TOKEN);
