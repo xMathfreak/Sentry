68d9e59cdd527ab4ab20cbc81ef28294ce55cdba
xMathfreak <36758660+xMathfreak@users.noreply.github.com>
2020-09-24 20:01:24 +0100

    Add files via upload

--- commands/define.js
+++ commands/define.js
@@ -7,7 +7,7 @@ module.exports = {
       description : "Searches for the definition of a word",
       usage : "`s!define [parameter]`"
     },
-    aliases : ["wordsearch", "definition"],
+    aliases : ["wordsearch", "definition", "def", "explains"],
     execute : async function(message, args){
       getDefinition(message, args);
     }
@@ -15,7 +15,7 @@ module.exports = {
 }
 
 function getDefinition(message, args){
-  if (!args[0]){return message.channel.send("**❌ You need to specify a word to search for!**").then(message => {message.delete(3000);}).catch();}
+  if (!args[0]){return message.channel.send("**❌ You need to specify a word to search for!**").then(message => {message.delete(6500);}).catch();}
 
   var options = {
     url : "https://api.dictionaryapi.dev/api/v2/entries/en/" + args,
@@ -31,22 +31,16 @@ function getDefinition(message, args){
 
     var data = JSON.parse(responseBody);
 
-    if (!data || !data[0]){return message.channel.send("**❌ There was an error getting the definition**").then(message => {message.delete(3000);}).catch();}
+    if (!data){return message.channel.send("**❌ There was an error getting the definition**").then(message => {message.delete(6500);}).catch();}
+    if (!data[0]){return message.channel.send("**❌ The word couldn't be found**").then(message => {message.delete(6500);}).catch();}
 
     var meaningsArr = data[0].meanings;
-    var definitionString = '';
+    var tempString = '';
 
     for(var definitions in meaningsArr){
       if (meaningsArr.hasOwnProperty(definitions)){
         var meaningIndex = parseInt(definitions) + 1;
-        var definitionsArr = meaningsArr[definitions].definitions;
-
-        for (var definition in definitionsArr){
-          if (definitionsArr.hasOwnProperty(definition)){
-            var definitionIndex = parseInt(definition) + 1;
-            definitionString = definitionString.concat(`${"**[" + meaningIndex + "-" + definitionIndex + "]** "}`,meaningsArr[definitions].definitions[definition].definition, `\n`);
-          }
-        }
+        tempString = tempString.concat(`${"**[" + meaningIndex + "]** "}`,meaningsArr[definitions].definitions[0].definition, `\n`);
       }
     }
 
@@ -59,7 +53,7 @@ function getDefinition(message, args){
         },
         {
           "name" : "Definitions",
-          value : `${definitionString}`
+          value : `${tempString}`
         }
       ]
     }});
