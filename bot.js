4352ed85f31345634d61a886f8514ab60aeedd59
xMathfreak <36758660+xMathfreak@users.noreply.github.com>
2020-09-30 02:57:27 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -2,17 +2,17 @@ const Discord = require('discord.js');
 const core = require('./include/core.js');
 
 const client = new Discord.Client();
-const prefix = 'sd!';
+const prefix = 's!';
 
 client.on('error', console.error);
 
 client.on('ready', () => {
   console.log(`Logged in as ${client.user.tag}!`);
-  client.user.setActivity("Sentry Dev-Test", {type : "LISTENING"});
+  client.user.setActivity("s!help | v0.9", {type : "LISTENING"});
 });
 
 client.on('message', async message => {
   core.executeCommand(message, prefix);
 });
 
-client.login('NDQ0OTE5OTMwNTc2NDM3Mjg2.Wvcp-Q.pH_QtIssKhWQ3zljWblHiX3G9Qk');
+client.login(process.env.BOT_TOKEN);
