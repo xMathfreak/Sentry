1511e3b86304172134a5012086bad00831f8475f
xMathfreak <36758660+xMathfreak@users.noreply.github.com>
2020-10-09 01:36:55 +0100

    Add files via upload

---
+++ commands/urban.js
@@ -0,0 +1,55 @@
+const request = require('request');
+
+module.exports = {
+  name: "urban",
+  help: {
+    name: "Urban Dictionary Search",
+    description: "Searches up a word from Urban Dictionary",
+    usage: "`s!urban [parameters]`"
+  },
+  category: "search",
+  aliases: ["urbandict", "urbandictionary", "udict", "udictionary", "urbansearch"],
+  execute: async function (message, args) {
+    getUrbanDefinition(message, args);
+  }
+}
+
+function getUrbanDefinition(message, args) {
+  if (!args[0]) return message.channel.send("**❌ You need to specify a word to search for!**");
+
+  var options = {
+    url: "http://api.urbandictionary.com/v0/define?term=" + args,
+    method: "GET",
+    headers: {
+      "Accept": "text/html",
+      "User-Agent": "Chrome"
+    }
+  };
+
+  request(options, function (error, response, responseBody) {
+    if (error) return;
+
+    var data = JSON.parse(responseBody);
+
+    if (!data) return message.channel.send("**❌ There was an error getting the definition**");
+    if (!data.list[0]) return message.channel.send("**❌ The word couldnt be found**");
+
+    message.channel.send({
+      embed: {
+        title: "",
+        fields: [{
+            "name": "Word",
+            "value": `${data.list[0].word}`
+          },
+          {
+            "name": "Definition",
+            value: `${data.list[0].definition}`
+          }
+        ],
+        footer: {
+          text: `Requested by: ${message.author.tag}`
+        }
+      }
+    });
+  });
+}
