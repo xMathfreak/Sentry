b3d1c85cfcca81c69f52ecc6fa0294f5acac544d
xMathfreak <36758660+xMathfreak@users.noreply.github.com>
2020-11-20 02:01:08 +0100

    Add files via upload

---
+++ avatar.js
@@ -0,0 +1,20 @@
+const { MessageEmbed } = require('discord.js');
+const { errorMessage } = require('../../utils/errors');
+
+module.exports = {
+  name: 'avatar',
+  aliases: ['pfp', 'profpic', 'icon'],
+  help: {
+    name: 'Avatar',
+    description: 'Fetches a users avatar',
+    usage: '`s!avatar [username]`',
+  },
+  category: 'utility',
+  execute: async function(message) {
+		let user = (!message.mentions.users.size) ? message.author : message.mentions.users.first();
+		const avatarEmbed = new MessageEmbed()
+			.setTitle(`${user.tag}'s Avatar`)
+			.setImage(user.avatarURL());
+		message.channel.send(avatarEmbed)
+  },
+};
